a45313d45e1afb7ebdef9166657c595dae0c246a
Arnaldo Francisco Vitaliano Filho <arnaldo.vitaliano@bcb.gov.br>
2016-07-18 13:48:43 -0300

    download ftp datafiles from IBGE/Brazil

--- scrapIBGE.R
+++ scrapIBGE.R
@@ -1,12 +1,10 @@
-# download endereços censo ibge
+# download enderecos censo ibge
 
 
 library(RCurl)
-# library(httr)
-# library(XML)
 dirs <- "ftp://anonymous:anonymous@ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Cadastro_Nacional_de_Enderecos_Fins_Estatisticos/"
-setwd("C:/Users/deinf.arnaldo/Desktop/ibge-setores-censitarios")
 
+# download lista de estados
 uf.list = unlist(strsplit(getURL(dirs, dirlistonly = TRUE), split = "\r\n"))
 uf.list <- uf.list[ uf.list != "Layout.zip" ]
 
@@ -19,22 +17,17 @@ download_folder_content <- function(basedir, subdir){
   files.names <- unlist(strsplit(getURL(dir.path, verbose = TRUE, dirlistonly = TRUE), split = '\r\n'))
 
   # constroi o link dos arquivos
-  files.path <- paste0(dir.path, files.name)
+  files.path <- paste0(dir.path, files.names)
 
   # define os nomes locais dos arquivos
-  local.names <- paste0(subdir, ".", files.names)
+  local.names <- paste0("Data/", subdir, ".", files.names)
 
   # faz o download
-  sapply(files.path, download.file, destfile = local.names)
+  #sapply(files.path, ?download.file, destfile = local.names)
+  mapply(download.file, url = files.path, destfile = local.names)
 
-  return(dir())
+  return(dir("Data"))
 }
 
-download_folder_content(dirs, 'AC')
-
-
-# uf.dir <- paste0(dirs, uf.list[1], "/")
-# files.name <- unlist(strsplit(getURL(uf.dir, verbose = TRUE, dirlistonly = TRUE), split = '\r\n'))
-# files.path <- paste0(uf.dir, files.name)
-# local.name <- paste0(uf.list[1], ".", files.name)
-
+# faz o download para todos os estados
+lapply(uf.list, download_folder_content, basedir = dirs)
