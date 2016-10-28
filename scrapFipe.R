660d3b32777edb43ad9d3ea9602f5042703a1d08
Arnaldo <arnaldo@AirVitaliano.local>
2016-10-28 10:41:03 -0200

    consegui baixar tabela de referencias de mes direto da api

--- scrapFipe.R
+++ scrapFipe.R
@@ -3,8 +3,25 @@
 library(httr)
 library(rvest)
 library(XML)
+library(jsonlite)
 
-fipe_baseurl <- "http://veiculos.fipe.org.br/"
+api_baseurl <- 'http://veiculos.fipe.org.br/api/veiculos'
+tabRef_api <- paste0(api_baseurl, "/ConsultarTabelaDeReferencia")        
 
-doc <- read_html(fipe_baseurl)
-str(doc)
+# Crédito destes header options 
+# Rafael Piza
+# http://phpbrasil.com/script/QqiPgvQWXrX3/importando-indices-da-fipe-preco-automoveis-para-seu-banco
+HEADER_OPTS <-  c('Accept' = 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding' = 'gzip, deflate',
+        'Accept-Language' = 'pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4',
+        'Connection' = 'keep-alive',
+        'Content-Type' = 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie' = '_ga=GA1.3.472052299.1466616166; _gat=1',
+        'Host' = 'veiculos.fipe.org.br',
+        'Origin' = 'http://veiculos.fipe.org.br',
+        'Referer' = 'http://veiculos.fipe.org.br/',
+        'User-Agent' = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+        'X-Requested-With' = 'XMLHttpRequest')
+
+tabRef <- content(POST(url = tabRef_api, config = add_headers(.headers = HEADER_OPTS), body = ""))
+unlist(tabRef)
