13e1dd2f334f28718e8202995fdcea05d9c30738
Arnaldo <arnaldo@AirVitaliano.local>
2016-10-30 00:06:36 -0200

    script completo. baixa todos os modelos das principais montadoras brasileiras.

--- scrapFipe.R
+++ scrapFipe.R
@@ -53,16 +53,23 @@ busca_mesReferencia <- function(){
 }
 
 # Busca as marcas
-busca_marcas <- function(tipo, mes.id, somente_principais = FALSE){
+busca_marcas <- function(tipo, mesRef, somente_principais = FALSE){
+        
         # parametros para chamar api
-        marcas_data <- list(codigoTabelaReferencia = mes.id, 
+        marcas_data <- list(codigoTabelaReferencia = mesRef$mes.id, 
                             codigoTipoVeiculo = tipo)
         # retorno
         marcas_return <- api_fipe(url = api_Marcas, marcas_data)
-        # jogando retorno em um data.frame
+        
+        # joga retorno em um data.frame
         marcas <- data.frame(matrix(unlist(marcas_return), ncol = 2, byrow = TRUE), 
                              stringsAsFactors = FALSE)
-        names(marcas) <- c("marca.nome", "marca.id")
+        
+        # concatena dados do mes de referencia
+        marcas <- cbind(mesRef$mes.id,
+                        mesRef$mes.nome,
+                        marcas)
+        names(marcas) <- c(names(mesRef), "marca.nome", "marca.id")
         
         if(somente_principais){
                 # seleciona somente as principais marcas no Brasil
@@ -77,41 +84,130 @@ busca_marcas <- function(tipo, mes.id, somente_principais = FALSE){
         return(marcas)
 }
 
-busca_modelos <- function(tipo, mes.id, marca.id){
+# Busca os modelos de uma marca
+busca_modelos <- function(tipo, marca){
         
-        modelos_data <- list( codigoTipoVeiculo = tipo,
-                              codigoTabelaReferencia = mes.id,
-                              codigoMarca = marca.id)
+        # parametros para chamar api
+        api_data <- list( codigoTipoVeiculo = tipo,
+                              codigoTabelaReferencia = marca$mes.id,
+                              codigoMarca = marca$marca.id)
         # codigoModelo = '',
         # ano = '',
         # codigoTipoCombustivel = '',
         # anoModelo = '',
         # modeloCodigoExterno = '')
-        modelos_return <- api_fipe(url = api_Modelos, data = modelos_data)
+        modelos_return <- api_fipe(url = api_Modelos, data = api_data)
         modelos <- modelos_return[['Modelos']]
         modelos <- data.frame(matrix(unlist(modelos), ncol = 2, byrow = TRUE), 
                               stringsAsFactors = FALSE)
         
-        names(modelos) <- c("modelo.name", "modelo.id")
+        # concatena informacoes da marca ao data frame
+        modelos <- cbind( marca, modelos)
+        
+        names(modelos) <- c(names(marca), "modelo.nome", "modelo.id")
         return(modelos)
 }
 
-# INIT
+# Busca os anos de um modelo
+busca_anos <- function(tipo, modelo){
+        
+        # parametros para chamar api
+        api_data <- list( codigoTipoVeiculo = tipo,
+                              codigoTabelaReferencia = modelo$mes.id,
+                              codigoMarca = modelo$marca.id,
+                              codigoModelo = modelo$modelo.id)
+        
+        anos_return <- api_fipe(url = api_Ano, data = api_data)
+        anos <- data.frame(matrix(unlist(anos_return), ncol = 2, byrow = TRUE), 
+                              stringsAsFactors = FALSE)
+        
+        # concatena informacoes da marca ao data frame
+        anos <- cbind( modelo, anos)
+        
+        names(anos) <- c(names(modelo), "ano.nome", "ano.id")
+        return(anos)
+}
 
-# seleciona meses disponíveis
-tabRef <- busca_mesReferencia()
+# Busca as informações de um modelo/ano específico
+busca_valor <- function(tipo, ano){
+        
+        anoModelo<- strsplit(ano$ano.id, split = "-")[[1]][1]
+        tipoCombustivel <- strsplit(ano$ano.id, split = "-")[[1]][2]
+        
+        # parametros para chamar api
+        api_data <- list( codigoTipoVeiculo = tipo,
+                          codigoTabelaReferencia = ano$mes.id,
+                          codigoMarca = ano$marca.id,
+                          codigoModelo = ano$modelo.id,
+                          ano = ano$ano.id,
+                          codigoTipoCombustivel = tipoCombustivel,
+                          anoModelo = anoModelo,
+                          tipoConsulta = 'Tradicional')
+        
+        valor <- api_fipe(url = api_Valor, data = api_data)
+        valor <- data.frame(t(unlist(valor)))
+        return(valor)
+}
 
-# Seleciona o mes atual, como o primeiro elemento da lista.
-mes_atual <- tabRef[1,]
-mes_atual
+download_fipe <- function(reset = FALSE, download_dir = "."){
+        
+        controle_arquivo <- paste0(download_dir, "/", "controleFipeDownload.RDS")
+        
+        # cria arquivo de controle se é primeira execução ou reset == TRUE
+        if( file.exists(controle_arquivo) && !reset){
+                print("Retomando download FIPE...")
+                controle <- readRDS(controle_arquivo)
+        }else{
+                # se nao, retoma download da marca que ainda não foi baixada.
+                print("Iniciando download FIPE...")
+                # seleciona meses disponíveis
+                tabRef <- busca_mesReferencia()        
+                
+                # Seleciona o mes atual, como o primeiro elemento da lista.
+                mes_atual <- tabRef[1,]
+                
+                # seleciona marcas
+                marcas <- busca_marcas(tipo = tipo['carro'], mesRef = mes_atual, 
+                                       somente_principais = TRUE)
+                
+                # constroi o data frame de controle dos downloads
+                controle <- marcas
+                controle$downloaded <- FALSE
+        }
+        
+        for(i in 1:nrow(controle)){
+                
+                marca_valores <- data.frame()
+                marca <- controle[i,]
+                
+                if(!marca$downloaded){
+                        print(paste("Baixando marca", marca$marca.nome))
+                        
+                        modelos <- busca_modelos(tipo = tipo['carro'], marca = marca )
+                        for(j in 1:nrow(modelos)){
+                                modelo <- modelos[j, ]
+                                print(paste("   Modelo",modelo$modelo.nome))
+                                
+                                anos <- busca_anos(tipo = tipo['carro'], modelo = modelo)
+                                for(k in 1:nrow(anos)){
+                                        ano <- anos[k,]
+                                        print (paste("      Ano", ano$ano.nome))
+                                        
+                                        valor <- busca_valor(tipo = tipo['carro'], ano = ano)
+                                        marca_valores <- rbind(marca_valores, valor)
+                                }
+                        }
+                        marca_arquivo <- paste0(download_dir, "/", marca$marca.nome,".csv")
+                        write.csv(marca_valores, file = marca_arquivo)
+                        
+                        # atualiza o arquivo de controle
+                        controle[i,]$downloaded <- TRUE
+                        saveRDS(controle, file = controle_arquivo)
+                }
+        }
+        # grava arquivo de controle
+        print("Download FIPE finalizado.")
+}
 
-# seleciona marcas
-marcas <- busca_marcas(tipo = tipo['carro'], mes.id = mes_atual$mes.id, somente_principais = TRUE)
-marcas
+download_fipe(download_dir = "data", reset = TRUE)
 
-# seleciona modelos de uma marca
-tipo <- tipo['carro']
-mes.id <- mes_atual$mes.id
-marca.id <- marcas[3, ]$marca.id
-modelos <- busca_modelos(tipo = tipo, mes.id = mes.id, marca.id = marca.id)
-View(modelos)
